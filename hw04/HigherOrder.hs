65a569443325f0bb01eb6d43bf1d1c66dbfd7952
Sergei Dolgov <dolgovs@gmail.com>
2016-04-28 20:15:11 +0200

    Solve 04.2

--- hw04/HigherOrder.hs
+++ hw04/HigherOrder.hs
@@ -17,3 +17,19 @@ fun1' = product . map (subtract 2) . filter even
 fun2' :: Integer -> Integer
 fun2' = sum . filter even . takeWhile (/=1) . iterate f
     where f n = if even n then n `div` 2 else 3*n + 1
+
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+    deriving (Show, Eq)
+
+foldTree :: [a] -> Tree a
+foldTree [] = Leaf
+foldTree xs =
+    Node height
+         (foldTree (take middle xs))
+         (xs !! middle)
+         (foldTree (drop (middle + 1) xs))
+    where
+        len = length xs
+        middle = len `div` 2
+        height = floor (logBase 2 (fromIntegral len))

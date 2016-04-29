8255062ac3b3be063ef79862feb55a3d6e88140f
Sergei Dolgov <dolgovs@gmail.com>
2016-04-29 07:15:30 +0200

    Solve 04.4

--- hw04/HigherOrder.hs
+++ hw04/HigherOrder.hs
@@ -1,5 +1,7 @@
 module HigherOrder where
 
+import Data.List
+
 fun1 :: [Integer] -> Integer
 fun1 [] = 1
 fun1 (x:xs)
@@ -33,3 +35,20 @@ foldTree xs =
         len = length xs
         middle = len `div` 2
         height = floor (logBase 2 (fromIntegral len))
+
+xor :: [Bool] -> Bool
+xor = foldr xor' False
+    where
+        xor' :: Bool -> Bool -> Bool
+        xor' a b = (a && not b) || (not a && b)
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\a -> (f a:)) []
+
+-- myFoldl :: (a -> b -> a) -> a -> [b] -> a
+-- myFoldl f base xs = foldr
+
+sieveSundaram :: Integer -> [Integer]
+sieveSundaram n = map ((+1) . (*2)) included
+    where excluded = filter (<=n) [i + j + 2 * i * j | j <- [1..n], i <- [1..j]]
+          included = [1..n] \\ excluded

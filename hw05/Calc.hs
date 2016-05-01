957dbfd1a15152c5ef17c17db3c7f05d0f871eb1
Sergei Dolgov <dolgovs@gmail.com>
2016-05-01 14:52:09 +0200

    Solve 05.2

--- hw05/Calc.hs
+++ hw05/Calc.hs
@@ -1,8 +1,17 @@
 module Calc where
 
 import ExprT
+import Parser
 
 eval :: ExprT -> Integer
 eval (Lit x) = x
 eval (Add a b) = eval a + eval b
 eval (Mul a b) = eval a * eval b
+
+evalStr :: String -> Maybe Integer
+evalStr s = case parseExp Lit Add Mul s of
+    Nothing -> Nothing
+    Just x -> Just (eval x)
+
+evalStr' :: String -> Maybe Integer
+evalStr' = fmap eval . parseExp Lit Add Mul

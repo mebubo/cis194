56b1a0dd50750a9d623bffe87ee388343fbd86b7
Sergei Dolgov <dolgovs@gmail.com>
2016-05-01 15:32:17 +0200

    Solve 05.3,4

--- hw05/Calc.hs
+++ hw05/Calc.hs
@@ -15,3 +15,48 @@ evalStr s = case parseExp Lit Add Mul s of
 
 evalStr' :: String -> Maybe Integer
 evalStr' = fmap eval . parseExp Lit Add Mul
+
+class Expr a where
+    lit :: Integer -> a
+    add :: a -> a -> a
+    mul :: a -> a -> a
+
+instance Expr ExprT where
+    lit = Lit
+    add = Add
+    mul = Mul
+
+reify :: ExprT -> ExprT
+reify = id
+
+instance Expr Integer where
+    lit = id
+    add = (+)
+    mul = (*)
+
+instance Expr Bool where
+    lit = (>0)
+    add = (||)
+    mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Eq, Show, Ord)
+
+instance Expr MinMax where
+    lit = MinMax
+    add = max
+    mul = min
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr Mod7 where
+    lit = Mod7
+    add (Mod7 a) (Mod7 b) = Mod7 . (`mod` 7) $ a + b
+    mul (Mod7 a) (Mod7 b) = Mod7 . (`mod` 7) $ a * b
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger = testExp :: Maybe Integer
+testBool = testExp :: Maybe Bool
+testMM = testExp :: Maybe MinMax
+testSat = testExp :: Maybe Mod7
